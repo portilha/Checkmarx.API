04dc21978feb4e22a4ee5236238473018aa0759f
Pedro Portilha <pedro.portilha@checkmarx.com>
2025-06-05 14:46:51 -0700

    refactor, removed unused methods and change the sleeps

--- Checkmarx.API/CxClient.cs
+++ Checkmarx.API/CxClient.cs
@@ -4189,20 +4189,6 @@ namespace Checkmarx.API
                 throw new ApplicationException(uploadResponse.ErrorMessage);
         }
 
-        //public void UpdateQueryGroup(CxAuditWebServiceV9.CxWSQueryGroup queryGroupToUpdate, long projectId, string description)
-        //{
-        //    if (queryGroupToUpdate == null)
-        //        throw new ArgumentNullException(nameof(queryGroupToUpdate));
-
-        //    queryGroupToUpdate.Status = CxAuditWebServiceV9.QueryStatus.Edited;
-        //    queryGroupToUpdate.PackageTypeName = $"CxProject_{projectId}";
-        //    queryGroupToUpdate.PackageFullName = $"{queryGroupToUpdate.LanguageName}:CxProject_{projectId}:{queryGroupToUpdate.Name}";
-        //    queryGroupToUpdate.ProjectId = projectId;
-        //    queryGroupToUpdate.Description = description;
-
-        //    UploadQueries([queryGroupToUpdate]);
-        //}
-
         public void DeleteQueryGroup(CxAuditWebServiceV9.CxWSQueryGroup queryGroupToDelete)
         {
             if (queryGroupToDelete == null)
